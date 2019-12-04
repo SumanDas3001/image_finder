a4eded1765777ebbd8046e64cd299a1825164962
SumanDas3001 <sumandas3001@gmail.com>
2019-12-04 12:44:10 +0530

    Updates

---
+++ precache-manifest.920b38563ce2edf6d65601531a5ae225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1b65954f846dc774f09f9b1bcd99ea",
+    "url": "/image_finder/index.html"
+  },
+  {
+    "revision": "7adc71d1d81a7a21dee3",
+    "url": "/image_finder/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ac126109386fbb238eb7",
+    "url": "/image_finder/static/js/2.e91884e8.chunk.js"
+  },
+  {
+    "revision": "7adc71d1d81a7a21dee3",
+    "url": "/image_finder/static/js/main.446b5f13.chunk.js"
+  },
+  {
+    "revision": "bfc82f0b37b83b44c837",
+    "url": "/image_finder/static/js/runtime-main.c8298d01.js"
+  }
+]);
